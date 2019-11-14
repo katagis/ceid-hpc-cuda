8a695510e3de795e8171c7771da770744f6471c4
katagis <fox.dealos@gmail.com>
2019-11-14 16:18:23 +0200

    Add dummy opt_cuda code

--- kernel.cu
+++ kernel.cu
@@ -37,7 +37,9 @@ Matrix cublas_Tmultiply(Matrix& inputMatrix) {
 #define AT(row, col, nr_cols) (row * nr_cols + col)
 
 
-
+//
+// SIMPLE CUDA
+//
 __global__ void basic_dev_Tmultiply(int nr_rows, int nr_cols, float* src, float* result)
 {
 	int row = threadIdx.x / nr_cols;
@@ -49,7 +51,7 @@ __global__ void basic_dev_Tmultiply(int nr_rows, int nr_cols, float* src, float*
 		sum += src[AT(i, row, nr_cols)] * src[AT(i, col, nr_cols)];
 	}
 
-	result[AT(row, col, nr_cols)] = sum + 1;
+	result[AT(row, col, nr_cols)] = sum;
 
 //	Index Debugger
 //	result[AT(row, col, nr_cols)] = row + 10 * col;
@@ -71,6 +73,10 @@ Matrix basic_Tmutliply(Matrix& input) {
 	return result;
 }
 
+
+
+
+Matrix opt_Tmutliply(Matrix& input);
 
 int main() {
 	Matrix inputMatrix(4, {
@@ -98,8 +104,66 @@ int main() {
 		basicCudaResult.Print();
 	}
 
+	Matrix optCudaResult;
+	optCudaResult = opt_Tmutliply(inputMatrix);
 
+	if (cublasResult.IsNearlyEqual(optCudaResult)) {
+		printf("Opt cuda was correct!\n");
+	}
+	else {
+		printf("Opt cuda was different:\n");
+		optCudaResult.Print();
+	}
 
 	cudaDeviceReset();
 	return 0;
+}
+
+
+//
+// OPTIMISED CUDA
+//
+
+//
+// Optimisation Notes: 
+// 1. T(A) * A is symmetrical
+// 2. 
+//
+//
+// Testing:
+// 1. Fill our 11GB GPU buffer without crashing the the OS & driver. 
+//    Check if offsets can be applied to make 1 buffer with multiple cuda memcopies.
+// 
+
+__global__ void opt_dev_Tmultiply(int nr_rows, int nr_cols, float* src, float* result)
+{
+	int row = threadIdx.x / nr_cols;
+	int col = threadIdx.x % nr_cols;
+
+	float sum = 0;
+
+	for (int i = 0; i < nr_cols; ++i) {
+		sum += src[AT(i, row, nr_cols)] * src[AT(i, col, nr_cols)];
+	}
+
+	result[AT(row, col, nr_cols)] = sum;
+
+	//	Index Debugger
+	//	result[AT(row, col, nr_cols)] = row + 10 * col;
+}
+
+Matrix opt_Tmutliply(Matrix& input) {
+	Matrix result(input.cols, input.cols);
+	result.AllocDevice();
+
+	input.IntoDevMatrix();
+
+	basic_dev_Tmultiply << <1, result.Size() >> > (input.rows, input.cols, input.dev_data, result.dev_data);
+
+	result.FromDevMatrix();
+
+	input.FreeDevice();
+	result.FreeDevice();
+
+	return result;
 }
